34c87fdafa5b43c21a0372aa72eb1c8bd7653786
Scott Robertson <scottr@activestate.com>
2019-08-16 19:27:31 -0700

    first commit

---
+++ intro.py
@@ -0,0 +1,12 @@
+execfile('recipe-580811-1.py')
+print("""
+UNO (TEXT-BASED) (PYTHON RECIPE)
+
+A text based recreation of the classic card game featuring functional AIs to play with.
+Some rules have been modified. User interface is text based, non-curses, using
+ only simple python commands to draw it.
+
+Type Uno() to start
+^D to exit
+To restart type: state activate recipes/580811-uno-text-based
+""")
